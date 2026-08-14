6425cb60b1e1a5cc8bf8ee2abdea6ee60fdaa537
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-14 16:32:55 +0300

    clientApi add register() login() interface UserDataRegister UserDataLogin and todos

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import type { CreateNote, Note } from "../../types/note";
+import { User } from "@/types/user";
+import { nextApi } from "./api";
 
 const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 const NOTES_PER_PAGE = 12;
@@ -14,6 +16,16 @@ const notesApi = axios.create({
 interface FetchNotesResponse {
   notes: Note[];
   totalPages: number;
+}
+
+export interface UserDataRegister {
+  email: string;
+  password: string;
+}
+
+export interface UserDataLogin {
+  email: string;
+  password: string;
 }
 
 export async function fetchNotes(
@@ -49,3 +61,19 @@ export async function deleteNote(id: string): Promise<Note> {
   const { data } = await notesApi.delete<Note>("/notes/" + id);
   return data;
 }
+
+export async function register(userData: UserDataRegister): Promise<User> {
+  const { data } = await nextApi.post<User>("/auth/register", userData);
+  return data;
+}
+
+export async function login(userData: UserDataLogin): Promise<User> {
+  const { data } = await nextApi.post<User>("/auth/login", userData);
+  return data;
+}
+
+// todo:
+// logout()
+// checkSession()
+// getMe()
+// updateMe()
