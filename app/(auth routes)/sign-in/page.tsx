4b2cffa0a77f3559a2b35ec3f87e82cfa432ac0b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-14 16:34:42 +0300

    add SignInPage html css and  handleSubmit

---
+++ app/(auth routes)/sign-in/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import css from "./SignInPage.module.css";
+import { login } from "@/lib/api/clientApi";
+
+export default function SignInPage() {
+  function handleSubmit(formData: FormData) {
+    const loginData = {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    };
+    login(loginData);
+  }
+
+  return (
+    <main className={css.mainContent}>
+      <form action={handleSubmit} className={css.form}>
+        <h1 className={css.formTitle}>Sign in</h1>
+
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Log in
+          </button>
+        </div>
+
+        <p className={css.error}>Error</p>
+      </form>
+    </main>
+  );
+}
