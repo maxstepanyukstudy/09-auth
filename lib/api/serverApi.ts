e58b4eca6fd9e528f4a083c6c4c3f2027565f6ab
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-16 20:40:47 +0300

    upd serverApi add fetchNotes fetchNoteById getMe checkSession

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,5 +1,58 @@
-// todo:
-// fetchNotes()
-// fetchNoteById()
-// getMe()
-// checkSession()
+import { User } from "@/types/user";
+import { FetchNotesResponse, nextApi } from "./api";
+import { cookies } from "next/headers";
+import { NOTES_PER_PAGE } from "../const";
+import { Note } from "@/types/note";
+
+export async function fetchNotes(
+  page?: number,
+  search?: string,
+  tag?: string,
+): Promise<FetchNotesResponse> {
+  tag = tag !== "all" ? tag : undefined;
+  const cookiesStore = await cookies();
+
+  const { data } = await nextApi.get<FetchNotesResponse>("/notes", {
+    params: {
+      page,
+      search,
+      tag,
+      perPage: NOTES_PER_PAGE,
+    },
+    headers: {
+      Cookie: cookiesStore.toString(),
+    },
+  });
+
+  return data;
+}
+
+export async function fetchNoteById(id: string): Promise<Note> {
+  const cookiesStore = await cookies();
+  const { data } = await nextApi.get<Note>("/notes/" + id, {
+    headers: {
+      Cookie: cookiesStore.toString(),
+    },
+  });
+  return data;
+}
+
+export async function getMe(): Promise<User> {
+  const cookiesStore = await cookies();
+  const { data } = await nextApi.get<User>("/users/me", {
+    headers: {
+      Cookie: cookiesStore.toString(),
+    },
+  });
+  return data;
+}
+
+export async function checkSession() {
+  const cookiesStore = await cookies();
+  const res = await nextApi.get("/auth/session", {
+    headers: {
+      Cookie: cookiesStore.toString(),
+    },
+  });
+  return res;
+}
