bfd05ee9ce549482fa3ea134c7688c6ca4169d4b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-17 00:25:07 +0300

    upd const set METADATA_OG_URL from env.NEXT_PUBLIC_API_URL

--- lib/const.ts
+++ lib/const.ts
@@ -12,7 +12,7 @@ export const APP_NOTES_FILTER_SLUG_PARAMS_INDEXES = {
   TAG_NAME: 0,
 };
 
-export const METADATA_OG_URL = "https://08-zustand-seven-pink.vercel.app"; // todo: upd after deploy
+export const METADATA_OG_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const METADATA_OG_IMG_URL =
   "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg";
